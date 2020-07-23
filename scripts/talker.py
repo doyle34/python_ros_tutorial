e1406532ac1b85ab743fc12edcf41678c0e89915
DONGIN <parkdi0409@gmail.com>
2020-07-23 20:36:06 +0900

    new branch

--- scripts/talker.py
+++ scripts/talker.py
@@ -4,16 +4,20 @@ import serial
 import rospy
 from std_msgs.msg import Float32
 
-ser = serial.Serial("/dev/ttyACM0")    #Open named port 
+ser = serial.Serial("/dev/ttyUSB1") # determine manually
 ser.baudrate = 115200
+
+def EBimuAsciiParser():
+    str_list = ser.readline().rstrip().split(',')
+    yaw = float(str_list[-1])
+    return yaw
 
 def talker():
     pub = rospy.Publisher('YawChatter', Float32, queue_size=50)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(20) # 10hz
+    rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        strdata = ser.readline().rstrip() # delete CRLF
-        yaw = float(strdata)
+        yaw = EBimuAsciiParser()
         pub.publish(yaw)
         rate.sleep()
 
